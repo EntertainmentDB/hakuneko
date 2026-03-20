0aa4cb9376e75e29596b222a76bd782882690aa0
xantasiadev <xantasiadev@proton.me>
2026-03-20 01:30:44 -0700

    Fix MangaKakalotFun: offset-based manga list pagination and pages format (#8460)
    * fix mangakakalotfun
    
    * address requested changes

--- src/web/mjs/connectors/MangaKakalotFun.mjs
+++ src/web/mjs/connectors/MangaKakalotFun.mjs
@@ -1,17 +1,12 @@
 import MangaHub from './MangaHub.mjs';
 
-/**
- *
- */
 export default class MangaKakalotFun extends MangaHub {
 
-    /**
-     *
-     */
     constructor() {
         super();
         super.id = 'mangakakalotfun';
         super.label = 'MangaKakalotFun';
+        this.tags = [ 'manga', 'webtoon', 'english' ];
         this.url = 'https://mangakakalot.fun';
 
         this.path = 'mn01';
